1afcbf78d720aadff08accec1f0993b256c189d0
lbx-8023 <3416944270@qq.com>
2025-07-29 09:53:25 +0800

    submit files

---
+++ examples/Interrupt/Interrupt.ino
@@ -0,0 +1,96 @@
+/*!
+ * @file interrupt.ino
+ * @brief This routine uses the IIC interface and adopts the interrupt mode to obtain data. 
+ * @n When the sensor data is ready, an interrupt signal will be generated.
+ * @n The obtained data include the current levels of PM1, PM2.5 and PM10 in the air.
+ * @n The demo supports FireBeetle-ESP32-E, FireBeetle-ESP32-S3, and FireBeetle-ESP8266.
+ * @details Experimental phenomenon: The read data will be output in the serial port monitor.
+ * 
+ * @copyright Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
+ * @license The MIT License (MIT)
+ * @author [Ouki](ouki.wang@dfrobot.com)
+ * @version V1.0
+ * @date 2025-07-28
+ * @url https://github.com/DFRobot/DFRobot_BMV080
+ */
+
+#include "DFRobot_BMV080.h"
+#include <string>
+
+SET_LOOP_TASK_STACK_SIZE(60 * 1024); // Set the stack size of the loop task to 60KB
+
+//You can choose to use either the IIC interface or the SPI interface. The default is IIC. 
+//If you want to use SPI, simply remove the following comments.
+DFRobot_BMV080_I2C sensor(&Wire, 0x57); // Create an instance of the DFRobot_BMV080_I2C class with the I2C address 0x57.
+
+//#define SPI_CS_PIN 17
+//DFRobot_BMV080_SPI sensor(&SPI,SPI_CS_PIN); // Create an instance of the DFRobot_BMV080_SPI class with the SPI CS pin.
+
+#define IRQ_Pin 14 // Define the interrupt pin.
+
+bool dataFlag = false; // Create a flag to indicate whether new data is available.
+
+void setup() {
+  char id[13]; // Variable to store the chip ID of the BMV080 sensor.
+  Serial.begin(115200);
+  while(!Serial) delay(100); // Wait for the serial port to be ready.
+  // Initialize the sensor.
+  Serial.println("The interrupt routine.");
+  while(sensor.begin() != 0){
+    Serial.println("Initialization of the sensor failed! Please confirm if the sensor chip connection is correct.");
+    delay(1000);
+  }
+  Serial.println("Initialization of the sensor was successful.");
+  // Open the BMV080 sensor.
+  while(sensor.openBmv080()){
+    Serial.println("open failed");
+    delay(1000);
+  }
+  Serial.println("open successful");
+  // Get the chip ID of the BMV080 sensor.
+  sensor.getBmv080ID(id);
+  Serial.println("Chip ID is:" + String(id));
+  // Set the measurement mode to continuous mode.
+  if(sensor.setBmv080Mode(DFRobot_BMV080_MODE_CONTINUOUS))
+    Serial.println("Mode setting successful");
+  // Set the interrupt pin.
+  setInterruptPin();
+}
+
+// define the variables to store the PM data.
+float pm1,pm2_5,pm10;
+void loop() {
+  // Check if new data is available.
+  // If the dataFlag is true, it means new data is available.
+  if(dataFlag){
+    dataFlag = false;
+    if(sensor.getBmv080Data(&pm1,&pm2_5,&pm10)){
+      Serial.print("pm1:" + String(pm1) + "  " + "pm2.5:" + String(pm2_5) + "  " + "pm10:" + String(pm10));
+
+      if(sensor.ifObstructed()){
+        Serial.print("  Obstructed The data may be invalid.");
+      }
+
+      Serial.println();
+    }
+  }
+  delay(100);
+}
+
+// Function to set the interrupt pin.
+// This function checks if the interrupt pin is supported on the board and sets it up.
+void setInterruptPin(void){
+  if(digitalPinToInterrupt(IRQ_Pin) == -1){
+    Serial.println("Interrupt pin not supported on this board.");
+  }else{
+    Serial.println("Interrupt pin supported.");
+  }
+  pinMode(IRQ_Pin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(IRQ_Pin), IRQ_handler, FALLING);
+}
+
+// Interrupt handler function
+void IRQ_handler(void)
+{
+  dataFlag = true;
+}
---
+++ src/DFRobot_BMV080.cpp
@@ -0,0 +1,396 @@
+#include "DFRobot_BMV080.h"
+
+DFRobot_BMV080::DFRobot_BMV080(void) {
+  // Constructor implementation 
+}
+
+int8_t DFRobot_BMV080::BMV080_write_16bit_cb(bmv080_sercom_handle_t sercom_handle, uint16_t header, const uint16_t* payload, uint16_t payload_length)
+{
+  DFRobot_BMV080 *_pDev = (DFRobot_BMV080 *)sercom_handle;
+  if(_pDev->devClass)
+    header = header << 1;
+  if(_pDev->writeReg(header, payload, payload_length) != 0){
+    DBG("writeReg failed");
+    return E_BMV080_ERROR_HW_WRITE; 
+  }
+
+  return E_BMV080_OK;
+}
+
+int8_t DFRobot_BMV080::BMV080_read_16bit_cb(bmv080_sercom_handle_t sercom_handle, uint16_t header, uint16_t* payload, uint16_t payload_length)
+{
+  DFRobot_BMV080 *_pDev = (DFRobot_BMV080 *)sercom_handle;
+  if(_pDev->devClass)
+    header = header << 1;
+
+  return _pDev->readReg(header, payload, payload_length) == payload_length ? E_BMV080_OK : E_BMV080_ERROR_HW_READ;
+}
+
+int8_t DFRobot_BMV080::BMV080_delay_cb(uint32_t duration_in_ms)
+{
+  delay(duration_in_ms);
+
+  return E_BMV080_OK;
+}
+
+uint32_t DFRobot_BMV080::BMV080_delay_cycling_cb(void)
+{
+  return millis();
+}
+
+void DFRobot_BMV080::getBmv080Data_cb(bmv080_output_t bmv080_output, void *cb_parameters)
+{
+  ((DFRobot_BMV080 *)cb_parameters)->get_bmv080Data(bmv080_output);
+}
+
+uint8_t DFRobot_BMV080::openBmv080(void)
+{
+  bmv080_status_code_t bmv080_status = bmv080_open(
+        &_bmv080_handle_class, (bmv080_sercom_handle_t)this, (bmv080_callback_read_t)BMV080_read_16bit_cb,
+        (bmv080_callback_write_t)BMV080_write_16bit_cb, (bmv080_callback_delay_t)BMV080_delay_cb);
+  //DBG("status is:" + String(bmv080_status));
+
+  return bmv080_status;
+}
+
+bool DFRobot_BMV080::closeBmv080(void)
+{
+  bmv080_status_code_t bmv080_status = bmv080_close(&_bmv080_handle_class);
+
+  return bmv080_status == E_BMV080_OK;  
+}
+
+bool DFRobot_BMV080::stopBmv080(void)
+{
+  bmv080_status_code_t bmv080_status = bmv080_stop_measurement(_bmv080_handle_class);
+
+  return bmv080_status == E_BMV080_OK;
+}
+
+bool DFRobot_BMV080::resetBmv080(void)
+{
+  bmv080_status_code_t bmv080_status = bmv080_reset(&_bmv080_handle_class);
+
+  if(bmv080_status != E_BMV080_OK) {
+    DBG("bmv080_reset failed, status is:" + String(bmv080_status));
+    return false;
+  }
+  return bmv080_status == E_BMV080_OK;
+}
+
+bool DFRobot_BMV080::getBmv080ID(char *id)
+{
+  bmv080_status_code_t bmv080_status = bmv080_get_sensor_id(_bmv080_handle_class, id);
+  
+  return (bmv080_status == E_BMV080_OK);
+}
+
+bool DFRobot_BMV080::getBmv080DV(uint16_t &major, uint16_t &minor, uint16_t &patch)
+{
+  char git_hash[12];
+  int32_t num_commits_ahead = 0;
+
+  bmv080_status_code_t bmv080_status = bmv080_get_driver_version(&major, &minor, &patch, git_hash, &num_commits_ahead);
+
+  return (bmv080_status == E_BMV080_OK);
+}
+
+bool DFRobot_BMV080::get_bmv080Data(bmv080_output_t bmv080_output)
+{
+  _bmv080Data = bmv080_output;
+  _bmv080DataOK = true;
+
+  return E_BMV080_OK;
+}
+
+bool DFRobot_BMV080::getBmv080Data(float *PM1, float *PM2_5, float *PM10)
+{
+  _bmv080DataOK = false;
+
+  if(_bmv080_handle_class == NULL) {
+    DBG("bmv080_handle_class is null 1");
+    return false;
+  }
+  bmv080_status_code_t bmv080_status =
+    bmv080_serve_interrupt(_bmv080_handle_class, (bmv080_callback_data_ready_t)getBmv080Data_cb, (void *)this);
+
+  if(bmv080_status != E_BMV080_OK) {
+    return false;
+  }
+  if(_bmv080DataOK){
+    *PM1 = _bmv080Data.pm1_mass_concentration;
+    *PM2_5 = _bmv080Data.pm2_5_mass_concentration;
+    *PM10 = _bmv080Data.pm10_mass_concentration;
+  }
+
+  return _bmv080DataOK;
+}
+
+bool DFRobot_BMV080::setBmv080Mode(uint8_t mode)
+{
+  bmv080_status_code_t bmv080_status = E_BMV080_ERROR_PARAM_INVALID_VALUE;
+
+  if(mode == DFRobot_BMV080_MODE_CONTINUOUS) {
+    bmv080_status = bmv080_start_continuous_measurement(_bmv080_handle_class);
+  }else if(mode == DFRobot_BMV080_MODE_DUTY_CYCLE) {
+    bmv080_duty_cycling_mode_t bmv080_duty_cycling_mode = E_BMV080_DUTY_CYCLING_MODE_0;
+    bmv080_status = bmv080_start_duty_cycling_measurement(_bmv080_handle_class, (bmv080_callback_tick_t)BMV080_delay_cycling_cb, bmv080_duty_cycling_mode);
+  } else {
+    DBG("setBmv080Mode failed, mode is invalid");
+    return false;
+  }
+
+  return (bmv080_status == E_BMV080_OK);
+}
+
+bool DFRobot_BMV080::setIntegrationTime(float integration_time)
+{
+  bmv080_status_code_t bmv080_status = bmv080_set_parameter(_bmv080_handle_class, "integration_time", (void *)&integration_time);
+
+  if(bmv080_status != E_BMV080_OK) {
+    DBG("setIntegrationTime failed, status is:" + String(bmv080_status));
+    return false;
+  }
+
+  return true;
+}
+
+float DFRobot_BMV080::getIntegrationTime(void)
+{
+  float integration_time = 0.0f;
+
+  bmv080_status_code_t bmv080_status = bmv080_set_parameter(_bmv080_handle_class, "integration_time", (void *)&integration_time);
+
+  if(bmv080_status != E_BMV080_OK) {
+    DBG("setIntegrationTime failed, status is:" + String(bmv080_status));
+    return false;
+  }
+
+  return integration_time;
+}
+
+bool DFRobot_BMV080::setDutyCyclingPeriod(uint16_t duty_cycling_period)
+{
+  bmv080_status_code_t bmv080_status =
+      bmv080_set_parameter(_bmv080_handle_class, "duty_cycling_period", (void *)&duty_cycling_period);
+
+  return (bmv080_status == E_BMV080_OK);
+}
+
+bool DFRobot_BMV080::getDutyCyclingPeriod(uint16_t *duty_cycling_period)
+{
+  bmv080_status_code_t bmv080_status = bmv080_get_parameter(_bmv080_handle_class, "duty_cycling_period", (void *)&duty_cycling_period);
+
+  return (bmv080_status == E_BMV080_OK ? duty_cycling_period : 0);
+}
+
+bool DFRobot_BMV080::setObstructionDetection(bool obstructed)
+{
+  bmv080_status_code_t bmv080_status = bmv080_set_parameter(_bmv080_handle_class, "do_obstruction_detection", (void *)&obstructed);
+
+  return (bmv080_status == E_BMV080_OK);
+}
+
+bool DFRobot_BMV080::getObstructionDetection(void)
+{
+  bool obstructed = false;
+  bmv080_status_code_t bmv080_status = bmv080_get_parameter(_bmv080_handle_class, "do_obstruction_detection", (void *)&obstructed);
+
+  return bmv080_status == E_BMV080_OK ? obstructed : false;
+}
+
+bool DFRobot_BMV080::ifObstructed(void)
+{
+  return _bmv080Data.is_obstructed;
+}
+bool DFRobot_BMV080::setDoVibrationFiltering(bool do_vibration_filtering)
+{
+  bmv080_status_code_t bmv080_status =
+      bmv080_set_parameter(_bmv080_handle_class, "do_vibration_filtering", (void *)&do_vibration_filtering);
+
+  return (bmv080_status == E_BMV080_OK);
+}
+
+bool DFRobot_BMV080::getDoVibrationFiltering(void)
+{
+  bool do_vibration_filtering = false;
+  bmv080_status_code_t bmv080_status = bmv080_get_parameter(_bmv080_handle_class, "do_vibration_filtering", (void *)&do_vibration_filtering);
+
+  return (bmv080_status == E_BMV080_OK ? do_vibration_filtering : false);
+}
+
+bool DFRobot_BMV080::setMeasurementAlgorithm(uint8_t measurement_algorithm)
+{
+  bmv080_measurement_algorithm_t bmv080_measurement_algorithm = (bmv080_measurement_algorithm_t)measurement_algorithm;
+  bmv080_status_code_t bmv080_status = bmv080_set_parameter(_bmv080_handle_class, "measurement_algorithm", (void *)&bmv080_measurement_algorithm);
+
+  return (bmv080_status == E_BMV080_OK);
+}
+uint8_t DFRobot_BMV080::getMeasurementAlgorithm(void)
+{
+  bmv080_measurement_algorithm_t measurement_algorithm;
+  bmv080_status_code_t bmv080_status = bmv080_get_parameter(_bmv080_handle_class, "measurement_algorithm", (void *)&measurement_algorithm);
+
+  return (bmv080_status == E_BMV080_OK ? (uint8_t)measurement_algorithm : 0);
+}
+
+DFRobot_BMV080_I2C::DFRobot_BMV080_I2C(TwoWire * Wire, uint8_t deviceAddr)
+{
+  _pWire = Wire;
+  _deviceAddr = deviceAddr;
+  devClass = true;
+}
+
+int DFRobot_BMV080_I2C::begin(void) 
+{
+  _pWire->begin();
+  _pWire->beginTransmission(_deviceAddr);
+  if(_pWire == NULL)
+  {
+    Serial.println("_pWire == NULL");
+    return ERR_DATA_BUS;
+  }
+  return _pWire->endTransmission() == 0 ? ERR_OK : ERR_DATA_BUS; 
+}
+
+uint16_t DFRobot_swap16(uint16_t i)
+{
+  #if defined(__clang__) || defined(__GNUC__)
+      return __builtin_bswap16(i);
+  #else
+      return (i << 8) | (i >> 8);
+  #endif
+}
+
+uint8_t DFRobot_BMV080_I2C::writeReg(uint16_t reg, const uint16_t* pBuf, size_t size)
+{ 
+  uint16_t data16[size];
+
+  for (size_t i = 0; i < size; i++)
+      data16[i] = DFRobot_swap16(pBuf[i]);
+  reg = DFRobot_swap16(reg);
+
+  if(pBuf == NULL){
+      DBG("pBuf ERROR!! : null pointer");
+      return 1;
+  }
+
+  _pWire->beginTransmission(_deviceAddr);
+  _pWire->write((uint8_t*)&reg, sizeof(reg));
+  _pWire->write((const uint8_t*)data16, size * sizeof(uint16_t));
+  uint8_t status = _pWire->endTransmission();
+
+  return status == 0 ? 0 : 1;
+}
+
+uint8_t DFRobot_BMV080_I2C::readReg(uint16_t reg, uint16_t* pBuf, size_t size)
+{
+  if (_pWire == NULL || pBuf == NULL) {
+    DBG("Invalid pointer: _pWire or pBuf is NULL");
+    return 0;
+  }
+
+  reg = DFRobot_swap16(reg);
+  _pWire->beginTransmission(_deviceAddr);
+  _pWire->write((uint8_t*)&reg, sizeof(reg));
+  if (_pWire->endTransmission() != 0) {
+    DBG("Failed to send register address");
+    return 0;
+  }
+
+  size_t totalBytes = size * sizeof(uint16_t);
+  uint8_t buffer[32];
+  uint16_t* pWord = pBuf;
+
+  while (totalBytes > 0) {
+    uint8_t chunkSize = (totalBytes > sizeof(buffer)) ? sizeof(buffer) : totalBytes;
+
+    _pWire->requestFrom(_deviceAddr, chunkSize);
+
+    for (uint8_t i = 0; i < chunkSize; i++) {
+      buffer[i] = _pWire->read();
+    }
+
+    for (uint8_t i = 0; i < chunkSize; i += sizeof(uint16_t)) {
+      *pWord = (buffer[i] << 8) | buffer[i + 1];
+      pWord++;
+    }
+    totalBytes -= chunkSize;
+  }
+
+  return size; 
+}
+
+
+DFRobot_BMV080_SPI::DFRobot_BMV080_SPI(SPIClass *spi, uint8_t csPin)
+{
+  _pSpi = spi;
+  _csPin = csPin;
+  devClass = false;
+}
+
+SPISettings spiSettings;
+
+int DFRobot_BMV080_SPI::begin(void) 
+{
+  if (_pSpi == NULL) {
+    DBG("_pSpi ERROR!! : null pointer");
+    return ERR_DATA_BUS;
+  }
+  pinMode(_csPin, OUTPUT);
+  _pSpi->begin();
+  spiSettings = SPISettings(1000000, MSBFIRST, SPI_MODE0);
+  return ERR_OK; 
+}
+
+uint8_t DFRobot_BMV080_SPI::writeReg(uint16_t reg, const uint16_t* pBuf, size_t size)
+{
+  if (_pSpi == NULL) {
+    DBG("pBuf ERROR!! : null pointer");
+    return 1;
+  }
+  
+  _pSpi->beginTransaction(spiSettings);
+  digitalWrite(_csPin, LOW);
+  _pSpi->transfer16(reg);
+  
+  while(size--) {
+    _pSpi->transfer16(*pBuf++);
+  }
+  
+  digitalWrite(_csPin, HIGH);
+  _pSpi->endTransaction();
+
+  return 0;
+}
+
+uint8_t DFRobot_BMV080_SPI::readReg(uint16_t reg, uint16_t* pBuf, size_t size) 
+{
+  if (_pSpi == NULL) {
+    DBG("_pSpi ERROR!! : null pointer");
+    return 0;
+  }
+
+  if (pBuf == NULL) {
+    DBG("pBuf ERROR!! : null pointer");
+    return 0;
+  }
+  uint16_t * _pBuf = (uint16_t *)pBuf;
+  size_t count = 0;
+
+  _pSpi->beginTransaction(spiSettings);
+  digitalWrite(_csPin, LOW);
+
+  _pSpi->transfer16(reg);
+
+  while(size--) {
+    *_pBuf = _pSpi->transfer16(0x00);
+    _pBuf++;
+    count++;
+  }
+
+   digitalWrite(_csPin, HIGH);
+  _pSpi->endTransaction();
+ 
+  return count;
+}
